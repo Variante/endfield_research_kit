e31697b10657cf04f0b422eefd86cc17f4603c59
Variante <382086919@qq.com>
2026-08-25 05:49:23 -0400

    Bake recovered Endminf solver topology

--- unity_endfield_graph_shader_lab/Assets/EndfieldGraphShaderLab/Runtime/Animation/EndfieldSecondaryDynamicsData.cs
+++ unity_endfield_graph_shader_lab/Assets/EndfieldGraphShaderLab/Runtime/Animation/EndfieldSecondaryDynamicsData.cs
@@ -20,6 +20,72 @@ namespace EndfieldGraphShaderLab
             public int baselineCount;
             public int centerFixedCount;
             public int colliderCount;
+
+            // Source-order proxy-mesh and prebuilt constraint data. These arrays are
+            // inert until a separately verified runtime elects to consume them.
+            public int[] referenceIndices;
+            public byte[] attributes;
+            public float[] vertexDepths;
+            public int[] vertexRootIndices;
+            public int[] vertexParentIndices;
+            public Vector3[] vertexLocalPositions;
+            public Quaternion[] vertexLocalRotations;
+            public Quaternion[] vertexToTransformRotations;
+            public byte[] baseLineFlags;
+            public ushort[] baseLineStartDataIndices;
+            public ushort[] baseLineDataCounts;
+            public ushort[] baseLineData;
+            public ushort[] centerFixedList;
+            public int[] distanceConstraintIndexArray;
+            public ushort[] distanceConstraintDataArray;
+            public float[] distanceConstraintRestLengths;
+
+            public SolverInputs solverInputs;
+        }
+
+        [Serializable]
+        public struct SolverInputs
+        {
+            public bool authoredScalarsRecovered;
+            public bool compiledCurveSamplesRecovered;
+            public string compiledCurveSamplesBoundary;
+
+            public int normalAxis;
+            public float gravity;
+            public Vector3 gravityDirection;
+            public float gravityFalloff;
+            public float animationPoseRatio;
+
+            public float dampingValue;
+            public bool dampingUsesCurve;
+            public float radiusValue;
+            public bool radiusUsesCurve;
+
+            public float inertiaDepth;
+            public bool particleSpeedLimitEnabled;
+            public float particleSpeedLimit;
+            public float centrifugalAcceleration;
+
+            public float tetherDistanceCompression;
+            public float distanceStiffnessValue;
+            public bool distanceStiffnessUsesCurve;
+
+            public bool angleRestorationEnabled;
+            public float angleRestorationStiffnessValue;
+            public bool angleRestorationStiffnessUsesCurve;
+            public float angleRestorationVelocityAttenuation;
+            public float angleRestorationGravityFalloff;
+            public bool angleLimitEnabled;
+            public float angleLimitValue;
+            public bool angleLimitUsesCurve;
+            public float angleLimitStiffness;
+
+            public float colliderDynamicFriction;
+            public bool springEnabled;
+            public float springPower;
+            public float springLimitDistance;
+            public float springNormalLimitRatio;
+            public float springNoise;
         }
 
         public bool sourceRecovered;
